9df5612e90de0c5bcc91344786e46108ee42516a
Angela0916 <138890205+Angela0916@users.noreply.github.com>
2023-09-12 18:12:37 +0800

    Week 2 assignment- simple app

---
+++ Week 2/Week 2 simple app/Week 2 simple app/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Week 2 simple app
+//
+//  Created by Angela  Pan on 2023/9/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+@IBOutlet weak var topLabel: UILabel!
+@IBOutlet weak var middleLabel: UILabel!
+@IBOutlet weak var button: UIButton!
+
+let text = [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus.",
+        "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+        "Richard McClintock, a Latin professor at Hampden-Sydney College in ",
+        "looked up one of the more obscure Latin words, consectetur",
+        "from a Lorem Ipsum passage, and going through the cities of the word",
+        "This book is a treatise on the theory of ethics, very popular during the.",
+        "The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.."
+    ]
+
+override func viewDidLoad() {
+    super.viewDidLoad()
+}
+@IBAction func buttonTapped(_ sender: UIButton) {
+    let randomColor = UIColor(red: CGFloat.random(in: 0...1),
+                              green: CGFloat.random(in: 0...1),
+                              blue: CGFloat.random(in: 0...1),
+                              alpha: 1.0)
+    let randomText = text.randomElement() ?? ""
+            middleLabel.text = randomText
+            view.backgroundColor = randomColor
+        }
+    }
